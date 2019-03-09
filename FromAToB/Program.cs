e0ea61a3aa3b3ae420d518a072e6748e3418eb13
eugene <Karate99kata>
2019-03-09 19:12:01 +0200

    Init commit

---
+++ FromAToB/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FromAToB
+{
+    /*
+     * a, b - input data value
+     * composition form a to b
+     * and result we must show on the display
+     **/
+    class Application
+    {
+        public static void Main()
+        {
+            Console.Write("a = ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("b = ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            int resultOfComposition = 1;
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                resultOfComposition = resultOfComposition * i;
+            }
+            
+            Console.WriteLine(
+                "Result of composition form " + 
+                firstNumber + " to " + secondNumber + 
+                " = " + resultOfComposition);
+        }
+    }
+}
---
+++ JulyTemperature/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace JulyTemperature
+{
+    /*
+     * У нас есть массив дней Июля
+     * Каждый елемент массива температура в этот день
+     * Посчитать среднюю температуру за месяц
+     **/
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int numberOfDays = 31;
+            int[] daysTemperature = new int[numberOfDays];
+            
+            Random rand = new Random();
+            
+            //Заполнение елементов случайно температурой
+            for (int i = 0; i < numberOfDays; i++)
+            {
+                daysTemperature[i] = rand.Next(15, 35);
+                Console.WriteLine(
+                    i+1 + " число = " + 
+                    daysTemperature[i] + " градусов Цельсия");
+            }
+            
+            //Сумма градусов за месяц
+            int sumOfTemperature = 0;
+            for (int i = 0; i < numberOfDays; i++)
+            {
+                sumOfTemperature = sumOfTemperature + daysTemperature[i];
+            }
+
+            float monthlyAverageTemperature = (float) sumOfTemperature / 31;
+            Console.WriteLine("Средняя температура за месяц = " + 
+                              monthlyAverageTemperature + " градусов Цельсия");
+        }
+    }
+}
